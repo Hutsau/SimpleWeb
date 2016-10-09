dc4650e35490427fc4ba7666cc6f95174e175329
Fortune <Aleksan9ep@gmail.com>
2016-10-09 12:08:41 +0300

    Правка EditPassword.xaml.cs

--- SimpleWeb/EditPassword.xaml.cs
+++ SimpleWeb/EditPassword.xaml.cs
@@ -22,6 +22,28 @@ namespace SimpleWeb
         public EditPassword()
         {
             InitializeComponent();
+
+            SaveBt.MouseDown += GlobalVars.mMouseDown;
+            CancelBt.MouseDown += GlobalVars.mMouseDown;
+
+            SaveBt.MouseUp += GlobalVars.mMouseUp;
+            CancelBt.MouseUp += GlobalVars.mMouseUp;
+
+            SaveBt.MouseLeave += GlobalVars.mMouseLeave;
+            CancelBt.MouseLeave += GlobalVars.mMouseLeave;
+
+            BadName.Visibility = Visibility.Hidden;
+            BadSurname.Visibility = Visibility.Hidden;
+            BadSecondName.Visibility = Visibility.Hidden;
+            BadLabel.Visibility = Visibility.Hidden;
+
+            CancelBt.MouseUp += (sender, e) => {
+                if (!GlobalVars.IsPressedFlag) return;
+
+                GlobalVars.IsPressedFlag = false;
+
+                this.Close();
+            };
         }
     }
 }
